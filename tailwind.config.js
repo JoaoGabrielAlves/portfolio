16487c1f81a74c4137c67b9fd457ce2d481f4af3
João Gabriel <joao@zapiet.com>
2022-08-16 11:50:42 -0300

    adding hover effect for buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         darkGunmetal: '#1D1E2B',
         gunMetal: '#2F3039',
         turquoise: '#41E4BD',
+        aquamarine: '#64FFDA',
         paleBlue: '#ADF1ED',
         water: '#C6FAF7',
         waterSpout: '#99FFFF',
